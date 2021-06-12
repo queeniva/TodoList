921761dd87e52ea0d44124fabfb0095820343517
Queen <pro-3is@Pro-3s-MacBook-Air.local>
2021-06-12 11:44:31 +0100

    i refactored the code

--- server.js
+++ server.js
@@ -1,52 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const date = require(__dirname + "/date.js")
+
 const app = express();
+
 var items = [];
 var workItem = [];
+
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static('public'));
 
 
 app.get("/", function (req, res) {
-
-    var today = new Date();
-    var options = {
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    }
-    var day = today.toLocaleDateString("en-US", options);
-
-    res.render("list", { listTitle: day, newListItem : items });
+    let day = date.getDate();
+    res.render("list", { listTitle: day, newListItem: items });
 });
 
 app.post("/", (req, res) => {
     var newItem = req.body.newItem;
-    if(req.body.list === "work"){
+    if (req.body.list === "work") {
         workItem.push(newItem)
         res.redirect("/work")
-    }else{
-    items.push(newItem)
-    res.redirect("/")
+    } else {
+        items.push(newItem)
+        res.redirect("/")
     }
 });
 
 app.get("/work", function (req, res) {
 
-    var today = new Date();
-    var options = {
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    }
-    var day = today.toLocaleDateString("en-US", options);
-
-    res.render("list", { listTitle: day, newListItem : workItem });
+    res.render("list", { listTitle: day, newListItem: workItem });
 });
 
-app.get("/about", function (req,res){
+app.get("/about", function (req, res) {
     res.render("about")
 });
 
