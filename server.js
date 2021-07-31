bbcd82893022fe26df512172196ed951334f7970
Queeniva <ngulefacbenazea@gmail.com>
2021-07-31 08:14:19 +0100

    iadded a backend to my todolist application using mongo db and mongoose

--- server.js
+++ server.js
@@ -1,37 +1,79 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const date = require(__dirname + "/date.js")
 
 const app = express();
-
-var items = [];
-var workItem = [];
-
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static('public'));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true })
+
+const itemSchema = {
+    name: String
+}
+
+const Item = mongoose.model("Item", itemSchema);
+const buy = new Item({
+    name: "buy bread"
+})
+const cook = new Item({
+    name: "cook beans"
+});
+const sell = new Item({
+    name: "sell umbrella"
+});
+
+const defaultArray = [buy, cook, sell]
 
 app.get("/", function (req, res) {
     let day = date.getDate();
-    res.render("list", { listTitle: day, newListItem: items });
+    Item.find({}, function (err, item) {
+        if (item.length === 0) {
+            Item.insertMany(defaultArray, function (err) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log("success")
+                }
+            });
+        } else {
+            res.render("list", { listTitle: day, newListItem: item });
+        }
+    });
 });
 
 app.post("/", (req, res) => {
     var newItem = req.body.newItem;
+
+    const item = new Item({
+        name: newItem
+    });
+    item.save();
     if (req.body.list === "work") {
         workItem.push(newItem)
         res.redirect("/work")
     } else {
-        items.push(newItem)
+        defaultArray.push(newItem)
         res.redirect("/")
     }
 });
 
-app.get("/work", function (req, res) {
+app.post("/delete", (req,res)=>{
+    const checkedItemId = req.body.checkbox ;
+    Item.findByIdAndRemove(checkedItemId, function(err){
+        if(!err){
+            console.log("success")
+            res.redirect("/")
+        }
+    })
+});
 
+app.get("/work", function (req, res) {
+    let day = date.getDate();
     res.render("list", { listTitle: day, newListItem: workItem });
 });
 
@@ -50,3 +92,4 @@ app.post("/work", (req, res) => {
 app.listen(3000, function () {
     console.log('server runing in port 3000');
 })
+
