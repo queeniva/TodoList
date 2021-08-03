cd06a69e07e68fa725543c36544d765101bd9c16
Queeniva <ngulefacbenazea@gmail.com>
2021-08-03 08:18:45 +0100

    add gitignore, Procfile and update ports

--- server.js
+++ server.js
@@ -131,9 +131,12 @@ app.post("/work", (req, res) => {
     res.redirect("/work")
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log('server runing in port 3000');
 })
 
