057a1c6d06270e5fc4bdaa79f47dcf0f1f7a1cb3
Queeniva <ngulefacbenazea@gmail.com>
2021-08-03 07:02:55 +0100

    tansfered my backend fro my local machine to the mongodb Atlas online

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static('public'));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:false })
+mongoose.connect("mongodb+srv://Admin_Queen:nguqueen1234@cluster0.nhk9p.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:false })
 
 const itemSchema = {
     name: String
