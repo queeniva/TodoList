cc2b22a201400494bdd5ab56dca27083f2c1a61c
Queeniva <ngulefacbenazea@gmail.com>
2021-08-05 16:57:25 +0100

    made the feild required

--- server.js
+++ server.js
@@ -10,11 +10,45 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static('public'));
+const empty = "pls input a todoItem"
 
-mongoose.connect("mongodb+srv://Admin_Queen:nguqueen1234@cluster0.nhk9p.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:false })
+// var qs = require("querystring");
+// var http = require("https");
+
+// var options = {
+//     "method": "GET",
+//     "hostname": "quotes15.p.rapidapi.com",
+//     "path": "https://quotes15.p.rapidapi.com/quotes/random/",
+//     "headers": {
+//         "x-rapidapi-key": "f11e519a9bmsh6d3c29dc4f5e985p1ce466jsnc6545e774d22",
+//     }
+// };
+
+// var req = http.request(options, function (res) {
+//     var chunks = [];
+
+//     res.on("data", function (chunk) {
+//         chunks.push(chunk);
+//     });
+
+//     res.on("end", function () {
+//         var body = Buffer.concat(chunks);
+//         var result = JSON.parse(body);
+//         console.log(result.content+result.originator.name);
+
+//     });
+// });
+
+// req.write(qs.stringify({}));
+// req.end();
+
+mongoose.connect("mongodb+srv://Admin_Queen:nguqueen1234@cluster0.nhk9p.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 
 const itemSchema = {
-    name: String
+    name: {
+        type: String,
+        require: true
+    }
 }
 
 const Item = mongoose.model("Item", itemSchema);
@@ -54,7 +88,7 @@ app.get("/", function (req, res) {
     });
 });
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
     const newItem = req.body.newItem;
     const listName = req.body.list;
     let day = date.getDate();
@@ -66,12 +100,12 @@ app.post("/", function(req, res) {
         res.redirect("/")
     } else {
         List.findOne({ name: listName }, function (err, foundList) {
-            if (err){
+            if (err) {
                 console.log(err)
-            }else{
-            foundList.items.push(item);
-            foundList.save();
-            res.redirect("/"+listName)
+            } else {
+                foundList.items.push(item);
+                foundList.save();
+                res.redirect("/" + listName)
             }
 
         })
@@ -85,15 +119,15 @@ app.post("/delete", (req, res) => {
     let day = date.getDate();
     if (listName === day) {
         Item.findByIdAndRemove(checkedItemId, function (err) {
-            if (!err) {            
+            if (!err) {
                 console.log("success")
                 res.redirect("/");
             }
         });
     } else {
-        List.findOneAndUpdate({name:listName},{$pull: {items:{_id:checkedItemId}}},function(err,result){
-            if(!err){
-                res.redirect("/"+listName);
+        List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkedItemId } } }, function (err, result) {
+            if (!err) {
+                res.redirect("/" + listName);
             }
         })
 
@@ -119,8 +153,6 @@ app.get("/:customLink", function (req, res) {
     })
 })
 
-
-
 app.get("/about", function (req, res) {
     res.render("about")
 });
@@ -133,7 +165,7 @@ app.post("/work", (req, res) => {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 3000;
+    port = 3000;
 }
 
 app.listen(port, function () {
